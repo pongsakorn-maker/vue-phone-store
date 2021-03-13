99476c43fb0c7c0349f2742d12fd9ed626cad9e2
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-14 00:33:24 +0700

    vue add router with history

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase/app'
+import router from './router'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -18,5 +19,6 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
